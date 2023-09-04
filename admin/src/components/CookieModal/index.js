0593ba6f43fcc815002259e84729cd57d8f33dc0
eigengrau <112807759+eigengrau-ch@users.noreply.github.com>
2023-09-04 16:30:14 +0200

    Revert "Fix error in the cookie modal during creation a new cookie"

--- admin/src/components/CookieModal/index.js
+++ admin/src/components/CookieModal/index.js
@@ -243,6 +243,7 @@ const Modal = ({ setShowModal, crudAction, categories, locale = null, preservedC
               defaultMessage: "Category"
             })}
             name="category"
+            required
             error={first(categoryValidation)}
             onChange={(value) => {
               handleValidation({ category: value }, setCategoryValidation)
@@ -266,6 +267,7 @@ const Modal = ({ setShowModal, crudAction, categories, locale = null, preservedC
               defaultMessage: "Party"
             })}
             name="party"
+            required
             error={first(partyValidation)}
             onChange={value => {
               handleValidation({ party: value }, setPartyValidation)
